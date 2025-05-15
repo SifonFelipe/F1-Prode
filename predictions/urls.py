ab67f3d4716e3fe86f79c5f24eb3fa20eea14ae5
SifonFelipe <supermegafelipe@gmail.com>
2025-05-15 19:58:55 -0300

    creating view and template

--- predictions/urls.py
+++ predictions/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create-pred/<str:season>/<str:location>/<str:session_type>', views.createPred, name="create-pred"),
     path("save_prediction/", views.save_pred, name="save_pred"),
     path("results-comp/<str:user>/<str:season>/<str:location>/<str:session_type>", views.compare_results, name="results-comparision"),
-    path("champions-pred/<str:season>", views.createPred, name='champions-pred')
+    path("champions-pred/<str:season>", views.championPred, name='champions-pred')
 ]
